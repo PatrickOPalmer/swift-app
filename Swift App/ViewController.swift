fb07f095cb3c75e070653fc29b96cc52cea913d8
Patrick Palmer <dieppe@yahoo.com>
2017-08-10 20:46:42 -0700

    no message

--- Swift App/ViewController.swift
+++ Swift App/ViewController.swift
@@ -11,22 +11,16 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var theLabel: UILabel!
-
-    var tapCount = 0
+    @IBOutlet weak var text1: UITextField!
+    @IBOutlet weak var text2: UITextField!
 
     @IBAction func buttonTapped(_ sender: Any) {
-        tapCount = tapCount + 1
-        print(tapCount)
-        if tapCount >= 10 {
-        theLabel.text = "You tapped the button \(tapCount) times!"
-        }
+        print(text1)
+        print(text1.text!)
+        print(text2.text!)
 
     }
 
-    @IBAction func buttonCool(_
-        sender: Any) {
-        theLabel.text = "Buttons are cool!"
-    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
